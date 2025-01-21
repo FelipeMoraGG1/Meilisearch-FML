6bb601b2103713c1e358fbc33ad9fc3c15dd689c
FelipeMoraGG1 <felipemora2345@gmail.com>
2025-01-20 22:01:29 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+// Importamos los estilos globales para la aplicación
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // El componente App es el contenedor principal de la aplicación Next.js.
+  // `Component` representa la página específica que se está cargando.
+  // `pageProps` contiene las propiedades que se pasan a esa página.
+  return <Component {...pageProps} />; // Renderizamos la página actual con sus propiedades.
 }
