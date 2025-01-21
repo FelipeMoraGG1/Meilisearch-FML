3191219fce3092a1d45f82dec691e468a117b63c
FelipeMoraGG1 <felipemora2345@gmail.com>
2025-01-20 22:03:38 -0600

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Ruta de API en Next.js. Más información en la documentación oficial:
+// https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  // Esta función maneja las solicitudes entrantes a esta ruta de API.
+  // `req` contiene la solicitud HTTP entrante.
+  // `res` se utiliza para enviar una respuesta al cliente.
+
+  res.status(200).json({ name: "John Doe" }); 
+  // Respuesta HTTP con código de estado 200 (éxito).
+  // Enviamos un objeto JSON como respuesta, en este caso con un campo `name`.
 }
